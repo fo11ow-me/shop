c6a481547e2d69f8220f09a0d2e89580cf029700
quuj <1473703258@qq.com>
2026-06-09 07:42:15 +0800

    feat: 增强全局异常处理
    新增 MethodArgumentNotValidException、IllegalArgumentException、Exception 全局处理器，返回规范错误响应

--- mall-server/src/main/java/com/qiujie/exception/BaseExceptionHandler.java
+++ mall-server/src/main/java/com/qiujie/exception/BaseExceptionHandler.java
@@ -4,11 +4,14 @@ import com.qiujie.dto.Response;
 import com.qiujie.dto.ResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ControllerAdvice   
+import java.util.stream.Collectors;
+
+@ControllerAdvice
 public class BaseExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(BaseExceptionHandler.class);
@@ -18,5 +21,28 @@ public class BaseExceptionHandler {
     public ResponseDTO handle(ServiceException exception){
         logger.info(exception.getMessage());
         return Response.error(exception.getCode(),exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResponseDTO handleValidation(MethodArgumentNotValidException e) {
+        String msg = e.getBindingResult().getFieldErrors().stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+        return Response.error(400, msg);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseBody
+    public ResponseDTO handleIllegalArgument(IllegalArgumentException e) {
+        logger.error("参数错误", e);
+        return Response.error(400, "请求参数不合法");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseDTO handleException(Exception e) {
+        logger.error("未处理异常", e);
+        return Response.error(500, "服务器内部错误");
     }
 }
